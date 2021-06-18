ef41f170b39a198715aeeb598b1d24309e42b910
Jon <jonwhite1993@gmail.com>
2021-06-17 21:53:40 -0600

    Added axios, refactored some components to functional components vs class components, added a spinner gif for loading animation, created app level state vs compo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import Navbar from './components/layout/Navbar';
-import './App.css';
 import Users from './components/users/Users';
 
+import './App.css';
+
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+  async componentDidMount() {
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/users?client=id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState({ users: res.data, loading: false})
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <div className="conatiner">
-        <Users/>
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,42 +1,26 @@
-import React, { Component } from "react";
-import UserItem from "./UserItem";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        login: "mojombo",
-        id: 1,
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        url: "https://api.github.com/users/mojombo",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        login: "defunkt",
-        id: 2,
-        avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-        url: "https://api.github.com/users/defunkt",
-        html_url: "https://github.com/defunkt",
-      },
-      {
-        login: "pjhyett",
-        id: 3,
-        avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-        url: "https://api.github.com/users/pjhyett",
-        html_url: "https://github.com/pjhyett",
-      }
-    ]
-  }
+import UserItem from './UserItem';
+import Spinner from '../misc/Spinner';
 
-  render() {
+const Users = ({ users, loading}) => {  
+  if (loading) {
+    return <Spinner />
+  } else {
     return (
-      <div className="grid-3">
-        {this.state.users.map( user => (
+      <div className='grid-3'>
+        {users.map( user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     )
   }
+}
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 }
 
 export default Users
